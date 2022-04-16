45a8e1532a679117c1b67d4ee95907665efdb119
Jamal Almonawer <jamalmonawer@gmail.com>
2022-04-16 12:56:29 +0200

    Number of processors

--- Performance-Measure/R2DBC/src/main/java/com/example/demo/DemoApplication.java
+++ Performance-Measure/R2DBC/src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,27 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class DemoApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DemoApplication.class);
 
     public static void main(String[] args) {
 
         SpringApplication.run(DemoApplication.class, args);
 
+    }
+
+    @PostConstruct
+    public void init() {
         // get the number of processors available to the Java virtual machine
-        int numberOfProcessors = Runtime.getRuntime().availableProcessors();
-        System.out.println("Number of processors available to this JVM: " + numberOfProcessors);
+        LOGGER.info("CPU: {}", Runtime.getRuntime().availableProcessors());
     }
 
 
