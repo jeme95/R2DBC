8e5d6b041f37715018b94864d9ec52bd0c15ff1b
Jamal Almonawer <jamalmonawer@gmail.com>
2022-04-16 12:42:05 +0200

    logging

--- Performance-Measure/R2DBC/src/main/java/com/example/demo/task/TaskController.java
+++ Performance-Measure/R2DBC/src/main/java/com/example/demo/task/TaskController.java
@@ -1,11 +1,16 @@
 package com.example.demo.task;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("api/tasks")
 public class TaskController {
+
+//    if you want to check which cores and threads are used
+//    private static final Logger LOGGER = LoggerFactory.getLogger(TaskController.class);
 
     private TaskService taskService;
 
@@ -16,6 +21,9 @@ public class TaskController {
 
     @GetMapping()
     public Flux<Task> getAll() {
+
+        //  if you want to check which cores and threads are used
+        //  return taskService.getAllTasks().doOnNext(task -> LOGGER.info("Server produces: {}", task));
         return taskService.getAllTasks();
     }
 
