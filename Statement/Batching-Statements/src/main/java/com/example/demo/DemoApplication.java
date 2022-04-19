4d44215172afe278c18cf2b8f5d4e8f4af0fea09
Jamal Almonawer <jamalmonawer@gmail.com>
2022-04-19 10:53:36 +0200

    eliminate blocking code

--- Statement/Batching-Statements/src/main/java/com/example/demo/DemoApplication.java
+++ Statement/Batching-Statements/src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,11 @@
 package com.example.demo;
 
-import io.r2dbc.spi.*;
 import org.mariadb.r2dbc.MariadbConnectionConfiguration;
 import org.mariadb.r2dbc.MariadbConnectionFactory;
 import org.mariadb.r2dbc.api.MariadbConnection;
-import org.mariadb.r2dbc.api.MariadbResult;
 import org.mariadb.r2dbc.api.MariadbStatement;
-import org.reactivestreams.Publisher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
@@ -20,26 +16,27 @@ public class DemoApplication {
 
 
         // establishing a Connection
-        MariadbConnection connection = createConnection();
+        Mono<MariadbConnection> connectionMono = createConnection();
+        connectionMono.subscribe(connection -> {
 
-        // validating the Connection
-        validateConnection(connection);
+            MariadbStatement updateStatement = connection.createStatement("UPDATE todo.tasks SET description=? WHERE  id=?;");
+            updateStatement.bind(0, "batching statement 1").bind(1, 3).add();
+            updateStatement.bind(0, "batching statement 2").bind(1, 4).add();
+            updateStatement.bind(0, "batching statement 3").bind(1, 5);
 
-        MariadbStatement updateStatement = connection.createStatement("UPDATE todo.tasks SET description=? WHERE  id=?;");
-        updateStatement.bind(0, "batching statement 1").bind(1,3).add();
-        updateStatement.bind(0, "batching statement 2").bind(1,4).add();
-        updateStatement.bind(0, "batching statement 3").bind(1,5);
+            // Retrieve and print the number of rows affected
+            updateStatement.execute().subscribe(result -> result.getRowsUpdated().
+                    subscribe(count -> System.out.println(count.toString())));
 
-        // Retrieve and print the number of rows affected
-        updateStatement.execute().subscribe(result -> result.getRowsUpdated().
-                subscribe(count -> System.out.println(count.toString())));
+        });
+
 
 //        keep the application running, otherwise the main thread may end before
 //        the result has been retrieved / before the statement was executed
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    private static MariadbConnection createConnection() {
+    private static Mono<MariadbConnection> createConnection() {
 
         MariadbConnectionConfiguration conf = MariadbConnectionConfiguration.builder()
                 .host("127.0.0.1")
@@ -51,21 +48,8 @@ public class DemoApplication {
 
         MariadbConnectionFactory connFactoryProg = new MariadbConnectionFactory(conf);
 
-        return connFactoryProg.create().block();
+        return connFactoryProg.create();
 
-    }
-
-
-    private static void validateConnection(MariadbConnection mariadbConnectionMono) {
-        Publisher<Boolean> validatePublisher = mariadbConnectionMono.validate(ValidationDepth.LOCAL);
-        Mono<Boolean> monoValidated = Mono.from(validatePublisher);
-        monoValidated.subscribe(validated -> {
-            if (validated) {
-                System.out.println("Connection is valid");
-            } else {
-                System.out.println("Connection is not valid");
-            }
-        });
     }
 
 
